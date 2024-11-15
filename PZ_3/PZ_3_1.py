c100905736fff2f9f3587a01bc2091f5867d8dd7
aniananas <ananas.zxc@yandex.ru>
2024-11-15 14:31:51 +0300

    Updated code

--- PZ_3/PZ_3_1.py
+++ PZ_3/PZ_3_1.py
@@ -1,14 +1,17 @@
 '''Вариант 13.
     Даны три целых числа: A, B, C. Проверить истинность высказывания: «Хотя бы одно
     из чисел A, B, C положительное».'''
-def intcheck(x, n):
+def intcheck(x, n:int):
     while type(x) != int:
         try:
             x = int(x)
             return x
         except ValueError:
-            print(f'Неправильно ввели {"первое" if n == 1 else "второе" if n==2 else "третье"} число!')
-            x = input(f'Введите {"первое" if n == 1 else "второе" if n==2 else "третье"} число: ')
-a, b, c = input('Введите первое число: '), input('Введите второе число: '), input('Введите третье число: ')
-a, b, c = intcheck(a, 1), intcheck(b, 2), intcheck(c, 3)
+            print(f'Неправильно ввели {"первое " if n == 1 else "второе " if n==2 else "третье " if n==3 else ""}число!')
+            x = input(f'Введите {"первое " if n == 1 else "второе " if n==2 else "третье " if n==3 else ""}число: ')
+
+a = intcheck(input('Введите первое число: '), 1)
+b = intcheck(input('Введите второе число: '), 2)
+c = intcheck(input('Введите третье число: '), 3)
+
 print("Одно из чисел положительное" if a>0 or b>0 or c>0 else "Все числа отрицательные")
