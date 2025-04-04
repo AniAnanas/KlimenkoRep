6af06ee738fd035d091ec1384027aedb1e07d817
aniananas <ananas.zxc@yandex.ru>
2025-04-04 17:49:20 +0300

    upd pz11_1

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -1,7 +1,26 @@
+from os import path
 from random import randint
 from typing import List 
+import logging
+
+strfmt = '[%(asctime)s][%(name)s][%(levelname)s] > %(message)s'
+datefmt = '%d-%m-%Y %H:%M:%S'
+formatter = logging.Formatter(fmt=strfmt, datefmt=datefmt)
+
+stream = logging.StreamHandler()
+stream.setFormatter(formatter)
+# stream.setLevel(logging.INFO)
+stream.setLevel(logging.DEBUG)
+
+logger = logging.getLogger("logg")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(stream)
 
 target_digits = -1
+
+MyDir = path.dirname(path.abspath(__file__))
+DigitsPath = path.join(MyDir, "digits.txt")
+TextOutputPath = path.join(MyDir, "output.txt")
 
 while target_digits < 1:
     try: 
@@ -9,30 +28,43 @@ while target_digits < 1:
         if target_digits > 0:
             break
         print("Кол-во чисел должно быть больше 0, повторите попытку")
-    except Exception as e: print("Неверно ввели число, повторите попытку.")
-
-with open("digits.txt", "w") as file1:
-    text = ""
-    for _ in range(0, target_digits):
-        text += str(randint(-100,100))+" "
-    file1.write(text)
-    print(f"[DEBUG] Файл {file1.name} создан с текстом: [{text}]")
+    except Exception as e: 
+        print("Неверно ввели число, повторите попытку.")
+        logger.error("Ошибка: {}".format(e.__str__()))
+try:
+    with open(DigitsPath, "w") as file1:
+        text = ""
+        for _ in range(0, target_digits):
+            text += str(randint(-100,100))+" "
+            
+        file1.write(text)
+        logger.debug(f"Файл {file1.name} создан с текстом: {text}")
+except Exception as e: logger.error("Не удалось записать файл, {}".format(e))
 
 def mult(a:List[int]) -> int:
-    print(f"[DEBUG] <function mult> вызвана со списком: {a}")
+    logger.debug(f"<function mult> вызвана со списком: {a}")
+    
     m = 1
     for el in a:
         m *= el
     return m
 
-with open("digits.txt", "r") as file1:
-    listd:List[int] = list(map(int, file1.readline().split()))
-    listlen = float(listd.__len__())
-    start_index, end_index =  int(listlen/3), int( listlen-(listlen/3))
-    with open("output.txt", "w") as output:
-        output.write(f"""Исходные данные: {listd}
+try:
+    with open(DigitsPath, "r") as file1:
+        listd:List[int] = list(map(int, file1.readline().split()))
+        listlen = float(listd.__len__())
+        start_index, end_index =  int(listlen/3), int( listlen-(listlen/3))
+        try:
+            with open(TextOutputPath, "w") as output:
+                text = f"""Исходные данные: {listd}
 Количество элементов: {int(listlen)}
 Индекс первого максимального элемента: {listd.index(max(listd))}
-Произведение элементов средней трети: {mult( listd[start_index:end_index] )}""")
+Произведение элементов средней трети: {mult( listd[start_index:end_index] )}"""
+                output.write(text)
+                
+                logger.debug(f"Создан файл в {path.join(MyDir, output.name)}\nТекст файла >>")
+                logger.info("\n"+text)
+        except Exception as e: logger.error("Не удалось записать файл, {}".format(e))
+except Exception as e: logger.error("Не удалось прочитать файл, {}".format(e))
 
-print("[DEBUG] Программа завершила работу.")
+logger.debug("Программа завершила работу.")
