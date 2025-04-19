1a8877616bea730a09a1c9a6cabd2058daf2b5b7
aniananas <ananas.zxc@yandex.ru>
2025-04-19 15:19:04 +0300

    Update PZ11

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -1,45 +1,34 @@
+"""Вариант 13.
+1. Средствами языка Python сформировать текстовый файл (.txt), содержащий
+последовательность из целых положительных и отрицательных чисел. Сформировать
+новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую
+обработку элементов:
+
+Исходные данные:
+Количество элементов:
+Индекс первого максимального элемента:
+Произведение элементов средней трети:
+"""
+
 from os import path
 from random import randint
 from typing import List 
 import logging
 
+#region Logging
 strfmt = '[%(asctime)s][%(name)s][%(levelname)s] > %(message)s'
 datefmt = '%d-%m-%Y %H:%M:%S'
 formatter = logging.Formatter(fmt=strfmt, datefmt=datefmt)
 
 stream = logging.StreamHandler()
 stream.setFormatter(formatter)
-# stream.setLevel(logging.INFO)
-stream.setLevel(logging.DEBUG)
+stream.setLevel(logging.INFO)
+# stream.setLevel(logging.DEBUG)
 
 logger = logging.getLogger("logg")
 logger.setLevel(logging.DEBUG)
 logger.addHandler(stream)
-
-target_digits = -1
-
-MyDir = path.dirname(path.abspath(__file__))
-DigitsPath = path.join(MyDir, "digits.txt")
-TextOutputPath = path.join(MyDir, "output.txt")
-
-while target_digits < 1:
-    try: 
-        target_digits = int(input("Введите количество чисел: ").strip())
-        if target_digits > 0:
-            break
-        print("Кол-во чисел должно быть больше 0, повторите попытку")
-    except Exception as e: 
-        print("Неверно ввели число, повторите попытку.")
-        logger.error("Ошибка: {}".format(e.__str__()))
-try:
-    with open(DigitsPath, "w") as file1:
-        text = ""
-        for _ in range(0, target_digits):
-            text += str(randint(-100,100))+" "
-            
-        file1.write(text)
-        logger.debug(f"Файл {file1.name} создан с текстом: {text}")
-except Exception as e: logger.error("Не удалось записать файл, {}".format(e))
+#endregion Logging
 
 def mult(a:List[int]) -> int:
     logger.debug(f"<function mult> вызвана со списком: {a}")
@@ -49,22 +38,52 @@ def mult(a:List[int]) -> int:
         m *= el
     return m
 
+targetDigits = -1
+
+scriptDir = path.dirname(path.abspath(__file__))
+digitsPath = path.join(scriptDir, "digits.txt")
+textOutputPath = path.join(scriptDir, "output.txt")
+
+while targetDigits < 1:
+    try: 
+        targetDigits = int(input("Введите количество чисел: ").strip())
+        if targetDigits > 0:
+            break
+        print("Кол-во чисел должно быть больше 0, повторите попытку")
+        
+    except ValueError: print("Неверно ввели число, повторите попытку.")
+    except KeyboardInterrupt: print("\r", end="") or exit()
+    except EOFError: print("\r", end="") or exit()
+    except Exception as e: 
+        print("Произошла ошибка, повторите попытку.")
+        logger.error(f"Ошибка: {e}")
+
 try:
-    with open(DigitsPath, "r") as file1:
+    with open(digitsPath, "w") as file1:
+        text = ""
+        for _ in range(0, targetDigits):
+            text += str(randint(-100,100))+" "
+            
+        file1.write(text)
+        logger.debug(f"Файл {file1.name} создан с текстом: {text}")
+except Exception as e: logger.error("Не удалось записать файл, {}".format(e))
+
+try:
+    with open(digitsPath, "r") as file1:
         listd:List[int] = list(map(int, file1.readline().split()))
         listlen = float(listd.__len__())
         start_index, end_index =  int(listlen/3), int( listlen-(listlen/3))
         try:
-            with open(TextOutputPath, "w") as output:
+            with open(textOutputPath, "w") as output:
                 text = f"""Исходные данные: {listd}
 Количество элементов: {int(listlen)}
 Индекс первого максимального элемента: {listd.index(max(listd))}
 Произведение элементов средней трети: {mult( listd[start_index:end_index] )}"""
                 output.write(text)
                 
-                logger.debug(f"Создан файл в {path.join(MyDir, output.name)}\nТекст файла >>")
-                logger.info("\n"+text)
-        except Exception as e: logger.error("Не удалось записать файл, {}".format(e))
-except Exception as e: logger.error("Не удалось прочитать файл, {}".format(e))
+                logger.debug(f"Создан файл в {path.join(scriptDir, output.name)}")
+                logger.info("Текст файла >>\n"+text)
+        except Exception as e: logger.error(f"Не удалось записать файл, ошибка: {e}")
+except Exception as e: logger.error(f"Не удалось прочитать файл, ошибка: {e}")
 
 logger.debug("Программа завершила работу.")
--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -1,49 +1,74 @@
+"""Вариант 13.
+2. Из предложенного текстового файла (text18-13.txt) вывести на экран его содержимое,
+количество символов в тексте. Сформировать новый файл, в который поместить текст в
+стихотворной форме предварительно вставив после строки N (N – задается пользователем)
+произвольную фразу.
+"""
+
 import time
 from os import path
 start = time.time()
 
-file_dir = path.dirname(path.abspath(__file__))
-path2file = path.join(file_dir, 'text18-13.txt')
+#region Config
+customText = "---Hello-World----"
+inputFileName = 'text18-13.txt'
+outputFileName = 'text_output_2.txt'
+#endregion 
 
-if not (path.exists(path2file)):
-    print(f"Файл по пути <{path2file}> не найден.")
+file_dir = path.dirname(path.abspath(__file__))
+InputFilePath = path.join(file_dir, inputFileName)
+OutputFilePath = path.join(file_dir, outputFileName)
+
+if not (path.exists(InputFilePath)):
+    print(f"Файл по пути <{InputFilePath}> не найден.")
     print("Создайте его в папке с исполняемым файлом")
     print("Выход..\r", end="")
     exit()
 
-with open(path2file, "r", encoding="UTF-16 LE") as f:
-    abc = f.read()
-    all_lines = abc.splitlines()
-    len_lines = len(all_lines)
+with open(InputFilePath, "r", encoding="UTF-16 LE") as f:
+    ReadedText:str = f.read()
+    allLines:list[str] = ReadedText.splitlines()
+    lenLines:int = len(allLines)
     
-    i=0
     print(f"Содержание файла:")
-    for line in all_lines:
+    i=0
+    content:list[str] = []
+    for l in allLines:
         i+=1
-        print(f"[{i}] | "+line)
-
-    num_line = -1
-    while num_line < 1 or num_line > len_lines:
+        content.append(f"[{i}] | " + l)
+    print("\n".join(content))
+    del content, i
+    
+    lineNum = -1
+    while lineNum < 1 or lineNum > lenLines:
         try:
-            num_line = int(input("\nВведите номер строки\n> ").strip())
-            
-            if num_line < 1:
+            lineNum = int(input("\nВведите номер строки\n> ").strip())
+            if lineNum < 1:
                 print("Номер строки должен быть больше 0, повторите попытку")
                 continue
-            
-            if num_line > len_lines:
-                print(f"Вы не можете ввести больше {len_lines}")
+            if lineNum > lenLines:
+                print(f"Вы не можете ввести больше {lenLines}")
                 continue
-            
             break
-        
         except ValueError: print("Неверно ввели число, повторите попытку.")
         except KeyboardInterrupt: print("\r", end="") or exit()
         except EOFError: print("\r", end="") or exit()
+        except Exception as e:
+            print(f"{e}\nПроизошла ошибка, повторите попытку.")
+    
+    stringBuilder:list[str] = []
+    
+    for i in range(1, lenLines+1):
+        text = allLines[i-1]
+        stringBuilder.append(str(text))
+        print(text)
+        
+        if i == lineNum:
+            stringBuilder.append(customText)
+            print(customText)
+            
+    with open(OutputFilePath, "w") as saveFile:
+        saveFile.write("\n".join(stringBuilder))
 
-    for i in range(1, len_lines+1):
-        print(all_lines[i-1])
-        if i == num_line:
-            print("---Hello-World----")
-
+    del stringBuilder, lineNum, allLines, lenLines
 print(f"\n\nПрограмма завершила работу. Затраченное время:{(time.time()-start):.3f}")
